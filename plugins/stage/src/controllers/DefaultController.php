2e8e4afbaa5e7c2a0d9d95dd1a9957f150476866
Roy <80959802+RoyScheepers@users.noreply.github.com>
2023-04-23 09:29:14 +0200

    #1

--- plugins/stage/src/controllers/DefaultController.php
+++ plugins/stage/src/controllers/DefaultController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Stage  plugin for Craft CMS 3.x
  *
@@ -13,31 +14,14 @@ namespace pwtstage\stage\controllers;
 
 use craft\web\Controller;
 
+use GuzzleHttp\Client;
+use craft\helpers\App;
 
-/**
- * Default Controller
- *
- * Generally speaking, controllers are the middlemen between the front end of
- * the CP/website and your plugin’s services. They contain action methods which
- * handle individual tasks.
- *
- * A common pattern used throughout Craft involves a controller action gathering
- * post data, saving it on a model, passing the model off to a service, and then
- * responding to the request appropriately depending on the service method’s response.
- *
- * Action methods begin with the prefix “action”, followed by a description of what
- * the method does (for example, actionSaveIngredient()).
- *
- * https://craftcms.com/docs/plugins/controllers
- *
- * @author    Roy 
- * @package   Stage
- * @since     1
- */
+
+
+
 class DefaultController extends Controller
 {
-
-
   // Protected Properties
   // =========================================================================
 
@@ -58,14 +42,29 @@ class DefaultController extends Controller
    * @return mixed
    */
 
-   /**
-    * api maken die een auto inlaadt   
-    * api maken die een model inlaadt gelinkt aan auto 
-    * api makne die generatie inlaadt gelinkt aan auto 
-    * api maken die motortype inlaadt gelinkt aan auto 
-    */
+  /**
+   * api maken die een auto inlaadt   
+   * api maken die een model inlaadt gelinkt aan auto 
+   * api makne die generatie inlaadt gelinkt aan auto 
+   * api maken die motortype inlaadt gelinkt aan auto 
+   */
+  public $apiKey;
+  public $apiBase;
+  public function __construct($config = [])
+  {
+    $this->apiBase = App::env('WEBSTEEN_API_URL');
+    $this->apiKey = App::env('WEBSTEEN_API_KEY');
+    $this->guzzleClient = new Client();
+  }
+
   public function actionIndex()
   {
-return true;
-}
-}
+    $apiURl = $this->guzzleClient->request('GET', $this->apiBase, [
+      'headers' => [
+        'Accept' => 'application/json',
+        'Authorization' => $this->apiKey
+      ]
+    ]);
+    return $apiURl;
+  }
+};
